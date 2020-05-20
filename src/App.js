e65515fc173599b5b5c13df9ab046a69b89c5141
thet4 <thomas.estiez@gmail.com>
2020-05-20 23:14:36 +0800

    created leaderboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import { Segment, Image, Grid } from "semantic-ui-react";
+import axios from "axios";
+const Header = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#e00785",
+        padding: 10,
+        width: "100%",
+        display: "inline",
+        zIndex: 1,
+        position: "fixed",
+        top: 0,
+        maxHeight: 120,
+      }}
+      columns={6}
+    >
+      <Grid>
+        <Grid.Column width={2}>
+          <Image
+            src="./logo.png"
+            size="small"
+            style={{ borderRadius: 100, minHeight: 50, minWidth: 50 }}
+          />
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <div style={{ fontSize: "2.5em", padding: "15%", color: "white" }}>
+            LEADERBOARD
+          </div>
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
+};
 
 function App() {
+  const [playerList, setPlayerList] = useState([]);
+
+  const http = axios.create({
+    baseURL: "https://campagne-simulator.herokuapp.com",
+  });
+
+  const getRankedPlayers = async () => {
+    try {
+      const newRankedPlayers = await http.get("/dashboard");
+      if (newRankedPlayers) setPlayerList(newRankedPlayers.data);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getRankedPlayers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div
+        style={{
+          padding: "20%",
+          paddingBottom: 10,
+          paddingTop: 130,
+        }}
+      >
+        {playerList.map((player, index) => {
+          return (
+            <Segment color="blue">
+              {index + 1} : {player.username} : {player.amount}$
+            </Segment>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
